e73b3a2615d3cbc975589fe9bd959edebde8ec91
thuyen04 <maivanthuyen07042004@gmail.com>
2023-12-22 00:10:46 +0700

    tìm kiếm

--- A/dssv.php
+++ A/dssv.php
@@ -1,5 +1,3 @@
-
-
 <?php
 session_start();
 if (!isset($_SESSION['user'])) {
@@ -17,19 +15,90 @@ if (!isset($_SESSION['user'])) {
           alert("bạn xóa sinh viên thành công ");
         </script> '; 
     }
+
+
+       $IdLop=[];
+       $sql="SELECT * FROM lop";
+       $result= $conn->query($sql);
+       $rows=0;
+       while($row=$result->fetch_assoc()) //doc tung dong cua ket qua
+            {
+                if (!(in_array($row['IdLop'],$IdLop))){
+                    $IdLop[$rows][0]= $row['IdLop'];
+                    $IdLop[$rows][1]= $row['MaLop'];
+                    $rows++;
+                }
+            }    
+
+
+
     mysqli_set_charset($conn,'UTF8');
-    if ($_SESSION['quyen']==1){
+    if ($_SESSION['quyen']==1  ){
+      
     $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop ";
     $result= $conn->query($sql);
+       if ( !($_GET['msv']=="") & isset($_GET['IdLop']) ) {
+        $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop WHERE `MSV` = ".$_GET['msv'];
+        $result= $conn->query($sql);
+      }
+      else if (!($_GET['msv']=="")){
+        $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop WHERE `MSV` = ".$_GET['msv'];
+        $result= $conn->query($sql);
+      }
+      else if (isset($_GET['IdLop'])){
+        $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop WHERE lop.`IdLop` = ".$_GET['IdLop'];
+        $result= $conn->query($sql);
+      }
+      else {
+        $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop ";
+        $result= $conn->query($sql);
+      }
+
+    
     }
+
+
     else if ($_SESSION['quyen']==2){
     $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop WHERE lop.MaLop='".$_GET['malop']."'";
     $result= $conn->query($sql);
-    }
+     if (!($_GET['msv']=="")){
+      $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop WHERE `MSV` = ".$_GET['msv'];
+      $result= $conn->query($sql);
+     }
+     else {
+      $sql="SELECT `MSV` , `Ten`,`TenLop` FROM thongtinsinhvien INNER JOIN lop ON lop.IdLop=thongtinsinhvien.IdLop ";
+      $result= $conn->query($sql);
+    }}
 ?>
 
 
 <div class="container mt-3">
+<form method="get" action="">
+    <div class="row">
+      <div class="col">
+        <input type="text" class="form-control" placeholder="mã sinh viên" name="msv">
+      </div>
+      <?php
+      if ($_SESSION['quyen']==1  ){
+      echo "
+      <div class='col'>
+        
+      <select class='form-select' id='malop' name='IdLop'>
+        <option selected disabled>chọn lớp</option>";
+
+      
+        foreach ($IdLop as $value) {
+            echo "<option value=".$value[0].">".$value[1]."</option>";
+          };
+      echo "
+      </select>
+      </div>";}
+      ?>
+      <div class="col">
+      <button type="submit" class="btn btn-primary" name='submit'>tìm kiếm</button>
+      </div>
+    </div>
+  </form>
   <h2>Danh Sách Sinh viên</h2>
               
   <table class="table table-striped">
@@ -55,7 +124,13 @@ if (!isset($_SESSION['user'])) {
             
         
             if ($_SESSION['quyen']==1){
-              echo "<td><a href='suasinhvien.php?MSV=".$row['MSV']."' style='text-decoration:none; color:green' >sửa    </a><a  href='dssv.php?idxoa=".$row['MSV']. "'style='text-decoration:none;color:red'>xóa</a></td>";
+              echo "<td><a href='suasinhvien.php?MSV=".$row['MSV']."' style='text-decoration:none; color:green ;padding-left: 25px;' ><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+              <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+              <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z'/>
+            </svg>    </a><a  href='dssv.php?idxoa=".$row['MSV']. "'style='text-decoration:none;color:red'><svg xmlns='http://www.w3.org/2000/svg' width='22' height='22' fill='currentColor' class='bi bi-person-x' viewBox='0 0 16 16'>
+              <path d='M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0M8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m.256 7a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1z'/>
+              <path d='M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708'/>
+            </svg></a></td>";
               }
             else if ($_SESSION['quyen']==2){
 
